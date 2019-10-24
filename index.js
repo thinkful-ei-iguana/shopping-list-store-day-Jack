4094fb2f7fc7ba86942a3df63cdc61197a0cf721
Jack <pupeljack@gmail.com>
2019-10-24 12:54:26 -0500

    figured it out lmao

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 'use strict';
 $(function() {
-  
-
-  /*jQuery('.thumbnail').on('click', e => {
-  console.log(e.target);
-  console.log(e.target.src);
-});
-
-
-
-jQuery('main').on('click', e => {
-  jQuery('.hero').children().attr('src', e.target.src);
-  jQuery('.hero').children().attr('alt', e.target.alt);
-  //make "hero" src and alt equal the thumbnail
-});  this all works dont mess with it too much*/
-
-  /*jQuery('main').on('keypress', e => {
-  var keycode = (event.keyCode ? event.keyCode : event.which);
-  if(keycode === '13'){
-    jQuery('.hero').children().attr('src', e.target.children().src);
-    jQuery('.hero').children().attr('alt', e.target.children().alt);
-  }
-});*/
-
-  /*$(document).keypress(function(event){ 
-    var keycode = (event.keyCode ? event.keyCode : event.which); 
-    if(keycode == '13'){ 
-      alert('You pressed a "enter" key in somewhere');
-    jQuery('.hero').children().attr('src', event.target.src);
-    jQuery('.hero').children().attr('alt', event.target.alt);
-    } 
-  });*/
+  $('.thumbnail').on('click', event =>{
+    event.preventDefault();
+    let srcAttr = $(event.currentTarget).find('img').attr('src');
+    let altAttr = $(event.currentTarget).find('img').attr('alt');
+    $('.hero').children().attr('src', srcAttr);
+    $('.hero').children().attr('alt', altAttr);  
+  });
 });
